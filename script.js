d6aa7ed93deb159a76b830b7b76d29867e5338f4
Leo Roos <leoroos88@gmail.com>
2026-02-20 15:50:34 +0100

    intial commit

---
+++ script.js
@@ -0,0 +1,196 @@
+var flagsDone = [];
+var data;
+var cur = -1;
+
+var flag;
+var template;
+var myGuess;
+var nextFlagBtn;
+var info;
+
+let canNextFlag;
+let input = "";
+const invalidKeys = [
+    "Alt",
+    "AltGraph",
+    "Control",
+    "Shift",
+    "Meta",      // Windows / Command key
+    "CapsLock",
+    "Tab", "Enter", "Backspace",
+    "Escape",
+    "Delete", "Insert",
+    "Home", "End", "PageUp", "PageDown",
+    "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"
+];
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    flag = document.getElementById("flag");
+    template = document.getElementById("flag-template");
+    myGuess = document.getElementById("my-guess");
+    nextFlagBtn = document.getElementById("next-flag");
+    info = document.getElementById("info");
+    
+    document.body.addEventListener("keydown", function(event) {
+        if (event.key == "Backspace") {
+            input = input.slice(0, -1);
+        }
+        else if (event.key == "Enter") {
+            if (canNextFlag) {
+                nextFlag();
+            }
+        }
+        else if (!invalidKeys.includes(event.key)) {
+            input += event.key;
+        }
+
+        myGuess.textContent = input;
+
+        if (input.toLowerCase() == data[cur].name.common.toLowerCase()) {
+            canNextFlag = true;
+            showFunFact();
+        }
+    })
+
+    nextFlagBtn.addEventListener("click", function() {
+        if (canNextFlag) {
+            nextFlag();
+        }
+    })
+
+    fetchData();
+})
+
+async function fetchData() {
+    let res = await fetch("https://restcountries.com/v3.1/all?fields=name,flags")
+    data = await res.json();
+    nextFlag();
+}
+
+function getNextFlag() {
+    const randomNr = Math.floor(Math.random() * data.length);    
+    if (flagsDone.includes(randomNr)) {
+        return getNextFlag();
+    }
+    return randomNr;
+}
+
+function nextFlag() {
+    canNextFlag = false;
+    hideFunFact();
+
+    const nextFlagNr = getNextFlag();
+    cur = nextFlagNr;
+    flagsDone.push(nextFlagNr);
+    const obj = data[nextFlagNr];
+
+    console.log(obj.name.common);
+    
+    const clone = template.content.cloneNode(true);
+    const img = clone.querySelector("img");
+    img.src = obj.flags.png;
+
+    flag.innerHTML = "";
+    flag.appendChild(clone);
+
+    input = "";
+    myGuess.textContent = "";
+
+    showFunFact();
+}
+
+function hideFunFact() {
+    info.classList.remove("show");
+
+}
+
+async function showFunFact() {
+    const res = await fetch(`https://restcountries.com/v3.1/name/${data[cur].name.common}`);
+    let countryData = await res.json();
+
+    let allData = countryData[0];
+
+    const countryName = document.getElementById("country-name");
+    countryName.textContent = `${allData.name.common} (${allData.name.official})`;
+
+    allData.tld = allData.tld || []
+    const topLevelDomain = document.getElementById("top-level-domain");
+    topLevelDomain.innerHTML = "";
+    for (let index = 0; index < allData.tld.length; index++) {
+        const tld = allData.tld[index];
+        const p = document.createElement("p");
+        p.textContent = tld;
+
+        topLevelDomain.append(p);
+    }
+    
+
+    allData.currencies = allData.currencies || {}
+    const currencies = document.getElementById("currencies");
+    currencies.innerHTML = "";
+    for (const key in allData.currencies) {
+        if (!Object.hasOwn(allData.currencies, key)) continue;
+        const currency = allData.currencies[key];
+        
+        const p = document.createElement("p");
+        p.textContent = `${currency.name} `;
+        
+        const span = document.createElement("span");
+        span.textContent = `(${currency.symbol})`;
+
+        p.append(span);
+        currencies.append(p);
+    }
+
+
+    console.log(allData.idd)
+    allData.idd = allData.idd || {}
+    const telephoneSuffix = document.getElementById("telephone-suffix");
+    telephoneSuffix.innerHTML = "";
+    for (let index = 0; index < allData.idd.suffixes.length; index++) {
+        const value = allData.idd.suffixes[index];
+        const p = document.createElement("p");
+        p.textContent = `${allData.idd.root} ${value}`;
+
+        telephoneSuffix.append(p);
+    }
+
+    
+    allData.capital = allData.capital || []
+    const capital = document.getElementById("capital");
+    capital.innerHTML = "";
+    for (let index = 0; index < allData.capital.length; index++) {
+        const value = allData.capital[index];
+        const p = document.createElement("p");
+        p.textContent = value;
+
+        capital.append(p);
+    }
+
+    const region = document.getElementById("region");
+    region.textContent = `${allData.region} (${allData.subregion})`;
+
+
+    allData.languages = allData.languages || {}
+    const languages = document.getElementById("languages");
+    languages.innerHTML = "";
+     for (const key in allData.languages) {
+        if (!Object.hasOwn(allData.languages, key)) continue;
+        const value = allData.languages[key];
+        
+        const p = document.createElement("p");
+        p.textContent = `${value}`;
+
+        languages.append(p);
+    }
+
+    const population = document.getElementById("population");
+    population.textContent = Intl.NumberFormat("sv-SE").format(allData.population);
+    
+
+    const coatOfArmsFlag = document.getElementById("coat-of-arms-flag");
+    coatOfArmsFlag.src = allData.coatOfArms.png;
+
+    info.classList.add("show");
+}
